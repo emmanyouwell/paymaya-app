aedf658add64a3dc0fbc38c47b558222d01d883f
emmanyouwell <emingala02@gmail.com>
2025-07-14 17:51:28 +0800

    Created NextJS Client for Electron App
    Co-authored-by: Jianella Guia Cabauatan <cabauatan.jianellaguia@gmail.com>

---
+++ client/app/form/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+const page = () => {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center">
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                        <CardAction>
+                            <Button variant="link">Sign Up</Button>
+                        </CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">Password</Label>
+                                        <a
+                                            href="#"
+                                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                        >
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    <Input id="password" type="password" required />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Login with Google
+                        </Button>
+                    </CardFooter>
+                </Card>
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+
+            </footer>
+        </div>
+    )
+}
+
+export default page
